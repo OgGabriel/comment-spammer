d118288d1ea3ef93ead50bbbbd59af17d5fbd807
) crypto addicted <71797980+OgGabriel@users.noreply.github.com>
2022-08-14 21:44:34 -0300

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -34,7 +34,7 @@ print(r'      |____/>> |_|     /_/   \_\ |_|  |_|  |_|  |_|  |_____|   |_| \_\  
 print(r'       )(  (__)||>>_    \\    >><<,-,,-.  <<,-,,-.   <<   >>   //   \\_ ') 
 print(r'      (__)    (__)__)  (__)  (__)(./  \.)  (./  \.) (__) (__) (__)  (__) ')
 print(' ')
-print('                                                                             author: hartz)
+print('                                                                             author: OgGabriel)
 print(' ')
 login = str(input(' [+] Coloque seu usuario do instagram: '))
 senha = getpass.getpass(' [+] Coloque a sua senha do instagram: ')
